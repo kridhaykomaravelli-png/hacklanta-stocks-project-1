1ea27d003994cc3f7d06aad92776c1f25e21034d
Kridhay Komaravelli <kridhaykomaravelli@gmail.com>
2026-09-13 12:34:59 -0400

    Update database.py

--- database.py
+++ database.py
@@ -8,6 +8,7 @@ def init_db():
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
 
+    
     cur.execute("""
         CREATE TABLE IF NOT EXISTS searches (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
